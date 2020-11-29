b8ce6b51bb1735d611c2119042ccf60c6a235f6d
lefatoum2 <christian.mbarga-mvogo@isen-ouest.yncrea.fr>
2020-11-29 18:26:22 +0100

    Initial2

--- ballgame.py
+++ ballgame.py
@@ -1,3 +1,12 @@
 import pygame
 BLACK = [0, 0, 0]
 WHITE = [0, 0, 0]
+GREEN = [0, 255, 0]
+RED = [255, 0, 0]
+BLUE = [0, 0, 255]
+
+pygame.init()
+
+TailleEcran = [400, 300]
+screen = pygame.display.set_mode(TailleEcran)
+pygame.display.set_caption("Super Ball")
